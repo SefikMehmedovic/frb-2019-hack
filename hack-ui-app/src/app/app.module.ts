22050712554a6d590538d00ad43d236c18ce816a
ljhwc3 <ljhwc3@mail.missouri.edu>
2019-08-05 16:05:40 -0500

    sign up UI

--- hack-ui-app/src/app/app.module.ts
+++ hack-ui-app/src/app/app.module.ts
@@ -3,14 +3,16 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { SignUpComponent } from './signup/sign-up.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
