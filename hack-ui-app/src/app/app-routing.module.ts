60e489a36a7b6061d38ffadb3c8d8eb5004969e8
SefikMehmedovic <27263031+SefikMehmedovic@users.noreply.github.com>
2019-08-06 10:16:18 -0500

    confrimation folder (#3)
    * confrimation folder
    
    * update project struct
    
    * confirmation page updates
    
    * minor changes
    
    * confirm page routing

--- hack-ui-app/src/app/app-routing.module.ts
+++ hack-ui-app/src/app/app-routing.module.ts
@@ -1,8 +1,16 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
+import { ConfirmationComponent } from './confirmation/confirmation.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+{
+  path: 'confirmation-page', component: ConfirmationComponent
+}
+
+
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
